53e41c28f6ad9abc69a2a1b20ce9ebb73dea067a
JackMechem <mechemjack@gmail.com>
2023-08-08 07:02:56 -0700

    Use Nextjs Fetch

--- src/lib/graphcms/client.ts
+++ src/lib/graphcms/client.ts
@@ -4,6 +4,7 @@ const graphcms = new GraphQLClient(`${process.env.GRAPH_CMS_ENDPOINT}`, {
     headers: {
         Authorization: `${process.env.GRAPH_CMS_PRODUCTION_AUTH}`,
     },
+    fetch,
 });
 
 export { graphcms };
